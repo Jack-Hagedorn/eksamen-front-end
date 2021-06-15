bbca2e485155566ec9d445819f0b5fd08be019ca
Jack <muniaxe@gmail.com>
2021-06-15 17:27:17 +0200

    This works

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,8 +2,8 @@ export const Welcome = () => {
 	return (
 		<div className="container">
 			<div className="content">
-				<h1>Hello World.</h1>
-				<p>This is the start of your project.</p>
+				<h1>Jack Hagedorn Jensen</h1>
+				<p>Datamatiker 3. Semester 2021 Forår, programerings eksamen</p>
 			</div>
 		</div>
 	);
--- src/pages/admin/AdminDashboard.js
+++ src/pages/admin/AdminDashboard.js
@@ -2,48 +2,92 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
 import { useEffect, useState } from "react";
+import { useAuth } from "../../providers/ProvideAuth";
 import { API_MAIN } from "../../utils/api";
 
 dayjs.extend(relativeTime);
 
 export const AdminDashboard = () => {
 	const [users, setUsers] = useState([]);
+	const [formData, setFormData] = useState({ projectname: "", projectdescription: "" })
 
 	useEffect(() => {
 		const doAsynchronousCall = async () => {
-			const response = await API_MAIN.get("admin/users");
+			const response = await API_MAIN.get("project/all");
 			setUsers(response.data);
 		};
 		doAsynchronousCall();
 	}, []);
+
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		console.log(formData.projectname)
+		console.log(formData.projectdescription)
+		await fetch('https://hagedorn.live/Eksamen/api/project', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				name: formData.projectname,
+				description: formData.projectdescription
+			})
+		}).then(res => {
+			return res.json()
+
+		})
+			.then(data => console.log(data))
+			.catch(error => console.log('ERROR'))
+	}
+
+
 
 	return (
 		<div className="container">
 			<div className="content">
 				<h1>Dashboard</h1>
 				<section>
-					<h2>Users</h2>
+					<h2>Projects</h2>
 					<p>Displaying all ({users.length}) users.</p>
 					<table className="table table-bordered">
 						<thead>
 							<tr>
-								<th>Username</th>
-								<th>Roles</th>
-								<th>Last updated</th>
-								<th>Created at</th>
+								<th>Project name</th>
+								<th>Description</th>
 							</tr>
 						</thead>
 						<tbody>
-							{users.map((user) => (
-								<tr>
-									<td>{user.username}</td>
-									<td>{user.roles.join(", ")}</td>
-									<td>{user.updatedAt ? dayjs(parseInt(user.updatedAt)).from() : "Never"}</td>
-									<td>{dayjs(parseInt(user.createdAt)).from()}</td>
+							{console.log(users)}
+							{users.map((project) => (
+								<tr key={project.id}>
+									<td>{project.name}</td>
+									<td>{project.description}</td>
 								</tr>
 							))}
 						</tbody>
 					</table>
+				</section>
+				<section>
+					<h2>Create new Project</h2>
+					<form onChange={handleChange} onSubmit={handleSubmit} className="w-400 mw-full">
+						<div className="form-group">
+							<label htmlFor="projectname" className="required">
+								Project Name
+							</label>
+							<input type="text" className="form-control" id="projectname" name="projectname" placeholder="Project Name" required="required" />
+						</div>
+						<div className="form-group">
+							<label htmlFor="projectdescription" className="required">
+								Project Description
+							</label>
+							<input type="text" name="projectdescription" className="form-control" id="projectdescription" placeholder="Project Description" required="required" />
+						</div>
+						<input className="btn btn-primary btn-block" type="submit" value="Create project" />
+					</form>
 				</section>
 			</div>
 		</div>
